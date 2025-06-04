dce6952a2e9d87e7f3300613a781ff22e58dd9bc
AliceBidzura <bidzurka@gmail.com>
2025-06-04 20:58:49 +0300

    Добавлен контейнер списка продуктов

---
+++ Shopping_List/ShoppingList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping_List
+{
+    public class ShoppingList
+    {
+        public DateTime Date { get; set; }
+        public List<Product> Products { get; set; }
+
+        public ShoppingList()
+        {
+            Date = DateTime.Now;
+            Products = new List<Product>();
+        }
+    }
+
+}
